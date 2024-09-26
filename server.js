21780097d103a48f5a8db33766ac47af9bcdab7a
gustavosxl <gustavo.sxlaio2334@gmail.com>
2024-09-26 15:36:38 -0300

    feat: create repositoryapi register-users

--- server.js
+++ server.js
@@ -3,55 +3,69 @@ import cors from 'cors'
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
-const app =  express()
+const app = express()
 
 app.use(express.json())
 app.use(cors())
 
-const port = process.env.PORT || 3000
 
 
-app.post('/users', async (req,res)=>{
+app.post('/users', async (req, res) => {
+    try {
+        const email = req.body.email;
+        
+        const existingEmail = await prisma.user.findUnique({
+            where: {
+                email: email
+            }
+        })
 
- await prisma.user.create({
-    data: {
-        name: req.body.name,
-        age: req.body.age,
-        email: req.body.email
+        if (existingEmail) throw new Error("There is a user using this email")
+
+        await prisma.user.create({
+            data: {
+                name: req.body.name,
+                age: req.body.age,
+                email: req.body.email
+            }
+
+        })
+        res.status(201).json(req.body)
+    } catch (err) {
+        return res.status(400).json({ error: err.message })
+    } finally {
+        console.log("finally correct")
     }
 
 })
 
-res.status(201).json(req.body)
-})
 
 
-
-app.get('/users',async (req,res) =>{
+app.get('/users', async (req, res) => {
 
     console.log(req)
 
     const users = await prisma.user.findMany()
 
     res.status(200).json(users)
-}) 
+})
 
-app.put('/users/:id', async (req,res) =>{
+app.put('/users/:id', async (req, res) => {
 
     await prisma.user.update({
         where: {
             id: req.params.id
         },
         data: {
-           name: req.body.name,
-           age: req.body.age, 
-           email: req.body.email 
+            name: req.body.name,
+            age: req.body.age,
+            email: req.body.email
         }
 
     })
 
 })
-app.delete('/users/:id', async (req,res) =>{
+app.delete('/users/:id', async (req, res) => {
 
     await prisma.user.delete({
         where: {
